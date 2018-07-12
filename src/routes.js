1efd3588cb3431c0c18525aa4ee162d7044bcbb3
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-12 09:30:56 +0800

    Create sanitization

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator/check');
+const { matchedData } = require('express-validator/filter');
 
 router.get('/', (req, res) => {
   res.render('index')
@@ -17,16 +18,21 @@ router.get('/contact', (req, res) => {
 router.post('/contact', [
   check('message')
     .isLength({ min: 1 })
-    .withMessage('Message is required'),
+    .withMessage('Message is required')
+    .trim(),
   check('email')
     .isEmail()
     .withMessage('That email doesn‘t look right')
+    .normalizeEmail()
 ], (req, res) => {
   const errors = validationResult(req);
   res.render('contact', {
     data: req.body,
     errors: errors.mapped()
   });
+
+  const data = matchedData(req);
+  console.log('Sanitized: ', data);
 });
 
 module.exports = router
